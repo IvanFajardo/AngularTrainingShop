d6aca55bf71ff23aff950b48cb99bb72ec099d98
Paul <pauladriancaga@gmail.com>
2021-10-06 10:15:51 +0800

    20211006PAC: fixed folder layout (2)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './Shared Components/login-page/login.component';
+import { LoginComponent } from './Main Components/login-page/login.component';
 
 const routes: Routes = [
   {
